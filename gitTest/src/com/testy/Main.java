92bb70eab12dbcf0c738081cf9546a1117e02cbd
yangtaochina <yangtaochina@163.com>
2017-01-25 09:44:43 +0800

    DEV分支开发的代码提交

--- gitTest/src/com/testy/Main.java
+++ gitTest/src/com/testy/Main.java
@@ -4,4 +4,12 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("aaa");
 	}
+	/**
+	 * DEV 分支建立编写的代码
+	 * @author YangTao 2017年1月25日 上午9:43:47
+	 * @return void
+	 */
+	public void util(){
+		System.out.println("util");
+	}
 }
